de5a1fb26930d00c9a2e618ecd18b81a2fc157c5
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-06 18:50:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,3 +9,8 @@ def line(katz_deli)
     puts "The line is currently: #{current_line.join(" ")}"
   end
 end
+
+def take_a_number(katz_deli, person)
+  katz_deli << person
+  puts "Welcome, #{person}. You are number"
+end
