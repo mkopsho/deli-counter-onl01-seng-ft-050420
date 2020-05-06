5588a7ae5584b881f303922eeb7672b650f424c7
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-06 18:39:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.each_
+    katz_deli.each_with_index do |e, i|
+      puts "The line is currently:"
     
 
 def take_a_number(katz_deli, name)
