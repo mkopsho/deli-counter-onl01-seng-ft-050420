d78297ce063d6bbbad0e26e1949a056202d5b424
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-06 18:57:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,5 +19,6 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Now serving #{katzdeli.shift}"
+    puts "Currently serving #{katzdeli.shift}."
+  end
 end
